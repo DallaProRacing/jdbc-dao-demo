692f96d4f7fa3e5beafb55b2f850402a60efcd41
DallaProRacing <103423@aluno.uricer.edu.br>
2024-10-25 19:20:10 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,6 +10,7 @@ public class Program {
 	public static void main(String[] args) {
 		
 		SellerDao sellerdao = DaoFactory.createSellerDao();
+		System.out.println("========= TESTE 1: FindByID");
 		Seller seller = sellerdao.findById(3);
 		
 		System.out.println(seller);
