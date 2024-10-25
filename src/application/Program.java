35c6c1f92e6811777886b1e26a6b3cf28c0266eb
DallaProRacing <103423@aluno.uricer.edu.br>
2024-10-25 16:04:26 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,17 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Program {
 
+	
+	public static void main(String[] args) {
+		
+		SellerDao sellerdao = DaoFactory.createSellerDao();
+		Seller seller = sellerdao.findById(3);
+		
+		System.out.println(seller);
+	}
 }
