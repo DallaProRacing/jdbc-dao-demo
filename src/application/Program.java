becb927f90cf74d944d5cf7115a7e0904d0ad1ec
DallaProRacing <103423@aluno.uricer.edu.br>
2024-10-27 19:54:44 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
-import model.dao.DaoFactory;
+import java.util.Date;
+
 import model.dao.SellerDao;
 import model.entities.Seller;
 
@@ -9,10 +10,5 @@ public class Program {
 	
 	public static void main(String[] args) {
 		
-		SellerDao sellerdao = DaoFactory.createSellerDao();
-		System.out.println("========= TESTE 1: FindByID");
-		Seller seller = sellerdao.findById(3);
-		
-		System.out.println(seller);
 	}
 }
